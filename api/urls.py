5c50e4be386ca9ea876ca7ed2584adce3a8a1ad9
Jordy van Zeeland <jordyvanzeeland@gmail.com>
2025-11-11 17:00:49 +0100

    Auth and register

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from .views import *
+from .components.auth import *
 
 urlpatterns = [
+    path('auth/login', login),
+    path('auth/register', register),
 ]

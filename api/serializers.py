fdd4e8c112887d92561fe9289a6f9332af991b84
Jordy van Zeeland <jordyvanzeeland@gmail.com>
2025-11-11 09:07:21 +0100

    initial setup

---
+++ api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from .models import Employment, EmploymentSkills, Projects, ProjectSkills, Skills, SkillsType
+
+class EmploymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employment
+        fields = '__all__'
+
+class ProjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = '__all__'
+
+class SkillsTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SkillsType
+        fields = '__all__'
+
+class SkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skills
+        fields = '__all__'
+
+class EmploymentSkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmploymentSkills
+        fields = '__all__'
+
+class ProjectSkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectSkills
+        fields = '__all__'
---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from django.views.decorators.csrf import csrf_exempt
+from .views import *
+
+urlpatterns = [
+]
